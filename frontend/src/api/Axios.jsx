9331ed6ac089b1301c4f1bf63d4839092d15eec9
Harshit <harshkush15sep@gmail.com>
2026-02-07 11:30:05 +0530

    backend deployed url

--- frontend/src/api/Axios.jsx
+++ frontend/src/api/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:2500/api",
+  baseURL: "https://threew-business-task-1.onrender.com/api",
 });
 
 api.interceptors.request.use(config => {
